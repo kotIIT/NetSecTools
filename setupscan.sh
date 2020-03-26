64a9d1a94678e403b5c66aff8f8e14a62691605c
kotIIT <mkotlyar@hawk.iit.edu>
2020-03-26 01:34:40 -0500

    created text file output for script scan

--- setupscan.sh
+++ setupscan.sh
@@ -1,56 +1,104 @@
 #!/bin/bash
 #automated enumeration
 #this script should perform automated scans by pulling network info from DHCP
-
 #what to do
-
 #get device IP adress
+myIP="$(hostname -I)"
+myInterface="$(ip -o -f inet addr show | awk '/scope global/ {print $2}')"
+mySubnet="$(ip -o -f inet addr show | awk '/scope global/ {print $4}')"
+outFile="Output/Devices.txt"
+IPList="Output/IPs.txt"
 
-getIP(){
-    echo IP address is
-    myIP="$(hostname -I)"
-    echo $myIP
-    echo
+getDeviceList()
+{
+    echo "Performing ARP Scan..."
+    sudo netdiscover -r $mySubnet  -PN > $outFile
+    sed  -i '$d' $outFile 
+    awk '{print $1} ' $outFile > $IPList
+    echo "Devices are:"
+    cat $outFile
+    
 }
-
-getInterface(){
-    echo "Interface name is"
-    myInterface = "ip -o -f inet addr show | awk '/scope global/ {print $2}')"
-    echo $myInterface
-}
-
-#getsubnet mask
-#Helps identify scope of the network
-getSubnet(){
-    echo "subnet mask is"
-    mySubnet="$(ip -o -f inet addr show | awk '/scope global/ {print $4}')"
-    echo $mySubnet 
-}
-
-
 
 
 #Perform a scripted network scan to find devices
 #Identify Open Ports
 #Identify Default Credentials
 
-ScanMySubnet(){
+ScanSubnet(){
+    File="Output/scannedlist.xml"
     echo 
     echo "Scanning Subnet"
-    nmap $mySubnet -sC -A --open -oX scannedlist.xml 
+    echo $mySubnet
+    nmap $mySubnet -sC -A --open  -A -oX  $outFile -webxml
+}
+
+ScanList(){
+    outFile="Output/list.xml"
+    echo 
+    echo "Scanning Listed Devices..."
+    nmap -iL  $IPList -sC --open -oX $outFile
+}
+
+Menu()
+{
+    echo "This is the menu of the script"
+    echo ""
+    echo "Enter 'ip' to find your IP and subnet"
+    echo "Enter 'iface' to list device interface"
+    echo "Enter the Port number to check if the port is open in the device list"
+    echo ""
+    echo "Press d. to listen for devices on the network"
+    echo "Press l. to display the devices on the network"
+    echo "Press s. to perform a scripted network scan  "
+    echo "press m to display menu again"
+    echo ""
+    echo ""
+    echo "To bypass this menu, enter the arguement before running the script"
+
 }
 
 
+ScanPort(){
+    File='Output/Port'$arg'.txt'
+    echo "Scanning for port" $arg
+    nmap -iL  $IPList  -p$arg --open -oG $File >/dev/null
+    gawk -i inplace '/open/{print $2} ' $File
+    sed -i '1d'  $File
+    cat $File
+    }
 
-Output(){
-    getIP
-    getInterface
-    getSubnet
-    ScanMySubnet
+
+makeChoice(){
+  echo ""
+
+  read -p "Select > " arg
+  case $arg in   
+    n|N) exit 0
+esac
 
 }
 
+arg=$1
 
+Output()
+{
+    case  $arg in
+    l)echo "List of devices on the network: " 
+        cat $IPList;;
+    ip) echo "IP address and subnet is" $myIP;;
+    iface) echo "Interface name is:" $myInterface;;
+    d) getDeviceList ;;
+    s) ScanSubnet ;;
+    $1 | m)  Menu;;
+    $arg) ScanPort ;;
+esac
 
+}
 
+while true :
+do
 Output
+makeChoice
+done
+
